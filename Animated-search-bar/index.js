8cd31bf22256de13f11d8b45e4e0427854cdb00d
Vishal Sharma <80852001+vishalsharma89@users.noreply.github.com>
2024-05-19 16:40:42 +0530

    Add files via upload
    Animated Search bar

---
+++ Animated-search-bar/index.js
@@ -0,0 +1,7 @@
+const searchBarConatinerEl= document.querySelector(".search-bar-container");
+
+const magnifierEl = document.querySelector(".magnifier");
+
+magnifierEl.addEventListener("click",()=>{
+    searchBarConatinerEl.classList.toggle("active");
+});
